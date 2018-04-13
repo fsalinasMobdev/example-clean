823cdcd6ffc5b8aec43888f09a3ffc3cea72bb58
Carlos Wistuba <cwistuba@optimisa.cl>
2018-04-13 16:49:36 -0300

    Creacion Carpetas Prueba

--- src/main/java/aplication/domain/usecase/ObtenerParticipanteUseCases.java
+++ src/main/java/aplication/domain/usecase/ObtenerParticipanteUseCases.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class ObtenerParticipanteUseCases {
 
     @Autowired
---
+++ src/test/java/aplication/domain/usecase/ObtenerParticipanteUseCasesTest.java
@@ -0,0 +1,7 @@
+package aplication.domain.usecase;
+
+import static org.junit.Assert.*;
+
+public class ObtenerParticipanteUseCasesTest {
+
+}
