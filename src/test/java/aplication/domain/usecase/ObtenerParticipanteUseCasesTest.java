271d4be29b25e2c13bdba8d717f7daf1fa43a15e
franciscosalinas <fsalinas@mobdev.cl>
2018-04-13 17:29:15 -0300

    prueba tomar set and get de lombok

---
+++ src/test/java/aplication/domain/usecase/ObtenerParticipanteUseCasesTest.java
@@ -0,0 +1,11 @@
+package aplication.domain.usecase;
+
+import aplication.domain.model.ParticipanteModel;
+
+public class ObtenerParticipanteUseCasesTest {
+    public void test() {
+        ParticipanteModel participanteModel = new ParticipanteModel();
+        participanteModel.setNombre("");
+
+    }
+}
